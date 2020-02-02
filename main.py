095c3e565263cc7731b3502f398eca4ebc960520
allanbz <allan.bezerra017@gmail.com>
2020-02-02 12:54:32 -0300

    Creating NN model and performing prediction

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-## Main
 from sklearn.model_selection import train_test_split
 from interval_func import create_intervals
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
 
 # Patient ID list according to database files
 patients_id = ['100', '101', '102', '103', '104', '105', '106', '107', '108', '109',
@@ -40,12 +41,35 @@ for _id, _input, _output in zip(ids, inputs, outputs):
 		_input.extend(intervals)
 		_output.extend(beat_types)
 
+# Converting to NumPy array for use in Keras
 training_input_dataset = np.array(training_input_dataset)
 training_output_dataset = np.array(training_output_dataset)
 testing_input_dataset = np.array(testing_input_dataset)
 testing_output_dataset = np.array(testing_output_dataset)
 
-print(training_input_dataset.shape)
-print(training_output_dataset.shape)
-print(testing_input_dataset.shape)
-print(testing_output_dataset.shape)
+# Expading output for standardization
+training_output_dataset = np.expand_dims(training_output_dataset, axis=1)
+testing_output_dataset = np.expand_dims(testing_output_dataset, axis=1)
+
+## Structuring Neural Network
+# Save input size for structuring purposes
+input_size = training_input_dataset.shape[1]
+output_size = training_output_dataset.shape[1]
+
+# Declares model
+model = Sequential()
+model.add(Dense(input_size * 2, input_shape = (input_size, ), activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(input_size * 2, activation = 'relu'))
+model.add(Dense(output_size, activation = 'sigmoid'))
+
+# Compiles model
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+# Training model
+model.fit(training_input_dataset, training_output_dataset, epochs = 1, verbose = 1)
+
+# Evaluating model
+score = model.evaluate(testing_input_dataset, testing_output_dataset, verbose = 1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
